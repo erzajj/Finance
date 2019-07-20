3e7b5754d9393cf02fe11c927cab30a8f040e834
Алексей <filimonovalekesy97@mail.ru>
2019-07-20 13:44:21 +0300

    dz lesson 7

---
+++ js/main.js
@@ -0,0 +1,41 @@
+let start = document.getElementById('start'),
+
+    //Блоки правой части
+    budgetValue = document.getElementsByClassName('budget-value'),
+    dayBudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+
+    //Поля input
+    expensesItem = document.getElementsByClassName('expenses-item'),
+
+    //Утвердить и рассчитать
+    Btns = document.getElementsByTagName('button'),
+    
+    //Поля input Необязательных расходов
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+
+    //Остальные поля через querySelector
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('.checksavings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+    
+    
+
+
+    
+
+    
+    
+
+    
+    
+
